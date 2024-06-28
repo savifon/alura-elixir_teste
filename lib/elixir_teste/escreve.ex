435ed0c3903590db4f2279f34018107d4eb82a58
savio.fonseca <savio.fonseca@ish.com.br>
2024-06-28 15:35:27 -0300

    documentando a tarefa no mix

--- lib/elixir_teste/escreve.ex
+++ lib/elixir_teste/escreve.ex
@@ -1,4 +1,13 @@
 defmodule Mix.Tasks.Escreve do
+    @moduledoc """
+    Documentação completa da tarefa
+
+    > iex mix escreve
+    """
+
+    use Mix.Task
+
+    @shortdoc "Escreve um número aleatório no arquivo.txt"
     def run(_) do
         ElixirTeste.EscreveNumeroAleatorio.escreve
     end
